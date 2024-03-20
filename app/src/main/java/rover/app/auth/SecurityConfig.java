323a13c4720d390cd47de5eb09c53ac24f4c164c
jitao <flmnix@gmail.com>
2024-03-20 09:50:58 +0800

    整理项目结构

--- app/src/main/java/rover/app/auth/SecurityConfig.java
+++ app/src/main/java/rover/app/auth/SecurityConfig.java
@@ -1,9 +1,9 @@
 package rover.app.auth;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig {
 }
