779bda3907dffbc62b166e984a43658f41fd460f
jitao <flmnix@gmail.com>
2024-03-25 13:16:07 +0800

    liquibase

---
+++ rover-db/src/main/java/rover/db/DbApp.java
@@ -0,0 +1,12 @@
+package rover.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbApp.class, args);
+    }
+}
