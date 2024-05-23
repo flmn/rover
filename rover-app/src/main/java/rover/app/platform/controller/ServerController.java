02efd9e5a3e76dbd8242bcc534668a0ad78dbc5a
jitao <flmnix@gmail.com>
2024-05-23 16:07:26 +0800

    ~

--- rover-app/src/main/java/rover/app/platform/controller/ServerController.java
+++ rover-app/src/main/java/rover/app/platform/controller/ServerController.java
@@ -1,5 +1,6 @@
 package rover.app.platform.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class ServerController {
     }
 
     @GetMapping("/info")
+    @PreAuthorize("hasAuthority('settings.server-info')")
     public Object list() {
         return serverInfoService.getServerInfo();
     }
--- rover-app/src/main/java/rover/app/shared/auth/SecurityConfig.java
+++ rover-app/src/main/java/rover/app/shared/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package rover.app.shared.auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +18,7 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration("appSecurityConfig")
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private static final RequestMatcher PROTECTED_URLS = new AndRequestMatcher(
             new AntPathRequestMatcher("/api/**"),
